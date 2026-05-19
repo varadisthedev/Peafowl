bbfc1fc235e22a29b48797c5757bb371e49e2be6
varadisthedev <varadisthedev@gmail.com>
2026-05-19 22:04:05 +0530

    refactor: move DB config into separate module

--- server/config/connectToMongoDB.js
+++ server/config/connectToMongoDB.js
@@ -3,17 +3,17 @@ import dotenv from "dotenv";
 dotenv.config();
 import chalk from "chalk";
 const log = console.log;
-const mongoURL = process.env.mongoURL;
+const mongoURL = process.env.MONGO_URI;
 if (!mongoURL) {
   console.error(
-    chalk.red("Error: mongoURL not defined in environment variables"),
+    chalk.red("Error: MONGO_URI not defined in environment variables"),
   );
   process.exit(1); // return a 1, clears mem, and exit process entirely
 }
 const connectMongo = async () => {
   try {
     await mongoose.connect(mongoURL);
-    log(chalk.green("#Connected to MongoDB"));
+    log(chalk.green("[MongoDB] Connected successfully!"));
   } catch (error) {
     log(chalk.red("Error connecting to MongoDB:", error));
     process.exit(1);
