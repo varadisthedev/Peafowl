128993bc6b0f92029ef1daafc106ce8b8141684b
varadisthedev <varadisthedev@gmail.com>
2026-05-25 00:56:15 +0530

    add: types for ts

---
+++ server/src/config/logger.ts
@@ -0,0 +1,20 @@
+// src/config/logger.js
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp(),
+        format.errors({ stack: true }),
+        format.json()
+    ),
+    transports: [
+        new transports.Console({
+            format: format.combine(format.colorize(), format.simple())
+        }),
+        new transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new transports.File({ filename: 'logs/combined.log' })
+    ]
+});
+
+module.exports = logger;
