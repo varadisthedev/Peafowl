158bc8a41558f06b8fa3b5f404b1bb497f58cf7c
varadisthedev <varadisthedev@gmail.com>
2026-07-08 19:16:41 +0530

    fix: fixed issues with poor connection with postgres and prisma initialization

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -1,6 +1,5 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
@@ -8,5 +7,6 @@ export default defineConfig({
   },
   datasource: {
     url: process.env["DATABASE_URL"],
+    // datasource.url: process.env["DATABASE_URL"] as string,
   },
 });
