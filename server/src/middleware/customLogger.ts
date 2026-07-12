e57c1fccf8563c3444e132a398ecbdff3e3e7259
varadisthedev <varadisthedev@gmail.com>
2026-07-13 00:48:20 +0530

    feat: minor changes in logging style to help in data analytics for future use. and improved file handling

--- server/src/middleware/customLogger.ts
+++ server/src/middleware/customLogger.ts
@@ -4,19 +4,33 @@ import type { Request, Response } from "express";
 import path from "path";
 
 const logsDir = path.join(process.cwd(), "logs");
-
+const logFile = path.join(logsDir, "access.log");
+// create folder if it doesnt exists
 if (!fs.existsSync(logsDir)) {
     fs.mkdirSync(logsDir, { recursive: true });
+    fs.appendFileSync(logFile, "Timestamp,IP,Method,URL,Status,Time\n");
+    // using csv is better for future usage in pandas or data analysis
+}
+// addign header on top
+try {
+    if (!fs.existsSync(logFile)) {
+        fs.writeFileSync(logFile, "Timestamp,IP,Method,URL,Status,Time\n");
+    }
+    else if (fs.statSync(logFile).size === 0) {
+        // using csv is better for future usage in pandas or data analysis
+        fs.appendFileSync(logFile, "Timestamp,IP,Method,URL,Status,Time\n");
+    }
+} catch (error) {
+    console.error("[ERROR] Error checking log file size, or file doenst exists: ", error);
 }
 
 export const logger = (req: Request, res: Response, next: NextFunction) => {
     const start = Date.now();
 
     res.on("finish", () => {
-        const log = `${new Date().toISOString()} | ${req.ip} | ${req.method} ${req.originalUrl} | ${res.statusCode} | ${Date.now() - start}ms\n`;
-
+        const log = `${new Date().toISOString()},${req.ip},${req.method},"${req.originalUrl}",${res.statusCode},${Date.now() - start}ms\n`;
         fs.appendFile(
-            path.join(logsDir, "access.log"),
+            logFile,
             log,
             error => {
                 if (error) {
